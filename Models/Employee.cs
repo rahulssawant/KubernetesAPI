5610289a9507c3ecdd351ececbc8a516ecbf6ad0
rahulssawant <rahul.sawant@citiustech.com>
2021-12-09 09:42:17 +0530

    Initial Commit

---
+++ Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AssignmentEmpAPIKubernetes.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string EmpName { get; set; }
+        public DateTime? DOB { get; set; }
+        public string Designation { get; set; }
+        public double Salary { get; set; }
+    }
+}
---
+++ Models/IEmployeeRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AssignmentEmpAPIKubernetes.Models
+{
+    public interface IEmployeeRepository
+    {
+        IEnumerable<Employee> GetAll();
+        Employee Get(int id);
+        Employee Add(Employee item);
+        void Remove(int id);
+        bool Update(Employee item);
+    }
+}
